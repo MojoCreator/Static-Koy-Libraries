3992019aeb4892f9b8d654212094f0e9eea68fbf
Mojo Developer Agent Four <51455479+mojodevagentfour@users.noreply.github.com>
2023-07-06 12:27:12 +0530

    Update train_util.py
    fix logging_dir issue

--- library/train_util.py
+++ library/train_util.py
@@ -2692,8 +2692,8 @@ def prepare_accelerator(args: argparse.Namespace):
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         mixed_precision=args.mixed_precision,
-        log_with=log_with,
-        logging_dir=logging_dir,
+        #log_with=log_with,
+        #logging_dir=logging_dir,
     )
 
     # accelerateの互換性問題を解決する
